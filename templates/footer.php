97571acb33b89899f30e403f46b7af7f5c4d7ec3
Jamie Spittal <spittal@kerplunc.com>
2014-07-02 10:20:43 -0700

    Don't need that script no-mo

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,4 @@
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
-
-<script type='text/javascript'>//<![CDATA[
-document.write("<script async src='//HOST:3000/browser-sync-client.1.1.2.js'><\/script>".replace(/HOST/g, location.hostname));
-//]]></script>
 <?php wp_footer(); ?>
